92d28845dc661eed4578044c3e6e2989a38be20c
Haakon Klausen <hakon.klausen@skatteetaten.no>
2017-03-20 09:02:52 +0100

    Version

--- cmd/version.go
+++ cmd/version.go
@@ -23,15 +23,10 @@ import (
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Shows the version of the aoc client",
+	Long: `Shows the version of the aoc client application`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Aurora OC version 4.0 alpha 1")
+		fmt.Println("Aurora OC version 4.0 alpha 2")
 	},
 }
 
