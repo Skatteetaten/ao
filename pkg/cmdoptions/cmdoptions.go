12ae35fe3a829f043d44e64bd3b92238304a22cd
HaakonKlausen <hakon.klausen@skatteetaten.no>
2017-05-30 12:39:55 +0200

    serverurl first version

--- pkg/cmdoptions/cmdoptions.go
+++ pkg/cmdoptions/cmdoptions.go
@@ -9,10 +9,11 @@ type CommonCommandOptions struct {
 	Localhost   bool
 	ShowConfig  bool
 	ShowObjects bool
+	ServerUrl   string
 }
 
 func (opt *CommonCommandOptions) ListOptions() (output string) {
-	output = fmt.Sprintf("Verbose: %v, Debug: %v, DryRun %v, Localhost: %v, ShowConfig: %v, ShowObject: %v",
-		opt.Verbose, opt.Debug, opt.DryRun, opt.Localhost, opt.ShowConfig, opt.ShowObjects)
+	output = fmt.Sprintf("Verbose: %v, Debug: %v, DryRun %v, Localhost: %v, ShowConfig: %v, ShowObject: %v, ServerUrl: %v",
+		opt.Verbose, opt.Debug, opt.DryRun, opt.Localhost, opt.ShowConfig, opt.ShowObjects, opt.ServerUrl)
 	return
 }
