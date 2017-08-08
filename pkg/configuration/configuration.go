6d773b2994dea9b08e1bc17f8375abf753a062c8
Håkon Klausen <hakon.klausen@icloud.com>
2017-08-08 11:48:19 +0200

    Features/aos 1708 deploy options (#15)
    * Allows deploy command to parse a mix of shortended env and app names
    Deploy --all
    Extended configuration object as a precursor to refactor common arguments
    
    * Fixed a few small bugs in deploy

--- pkg/configuration/configuration.go
+++ pkg/configuration/configuration.go
@@ -2,51 +2,70 @@ package configuration
 
 import (
 	"errors"
+	"github.com/skatteetaten/aoc/pkg/cmdoptions"
 	"github.com/skatteetaten/aoc/pkg/openshift"
 	"github.com/spf13/viper"
 )
 
 type ConfigurationClass struct {
-	configLocation  string
-	openshiftConfig *openshift.OpenshiftConfig
-	apiClusterIndex int
-	apiClusterName  string
-	initDone        bool
+	configLocation    string
+	openshiftConfig   *openshift.OpenshiftConfig
+	apiClusterIndex   int
+	apiClusterName    string
+	persistentOptions *cmdoptions.CommonCommandOptions
+	initDone          bool
 }
 
-func (configurationClass *ConfigurationClass) init() (err error) {
-	if configurationClass.initDone {
+func (configuration *ConfigurationClass) Init(persistentOptions *cmdoptions.CommonCommandOptions) (err error) {
+	configuration.persistentOptions = persistentOptions
+	configuration.init()
+	return
+}
+
+func (configuration *ConfigurationClass) init() (err error) {
+	if configuration.initDone {
 		return
 	}
-	configurationClass.configLocation = viper.GetString("HOME") + "/.aoc.json"
-	configurationClass.openshiftConfig, err = openshift.LoadOrInitiateConfigFile(configurationClass.configLocation, false)
+
+	configuration.configLocation = viper.GetString("HOME") + "/.aoc.json"
+	configuration.openshiftConfig, err = openshift.LoadOrInitiateConfigFile(configuration.configLocation, false)
 	if err != nil {
 		err = errors.New("Error in loading OpenShift configuration")
 	}
 	// Find index for API cluster,that is the first reachable cluster
-	if configurationClass.openshiftConfig != nil {
-		for i := range configurationClass.openshiftConfig.Clusters {
-			if configurationClass.openshiftConfig.Clusters[i].Name == configurationClass.openshiftConfig.APICluster {
-				configurationClass.apiClusterIndex = i
+	if configuration.openshiftConfig != nil {
+		for i := range configuration.openshiftConfig.Clusters {
+			if configuration.openshiftConfig.Clusters[i].Name == configuration.openshiftConfig.APICluster {
+				configuration.apiClusterIndex = i
 				break
 			}
 		}
 	}
-	configurationClass.initDone = true
+	configuration.initDone = true
 	return
 }
 
-func (configurationClass *ConfigurationClass) GetOpenshiftConfig() *openshift.OpenshiftConfig {
-	configurationClass.init()
-	return configurationClass.openshiftConfig
+func (configuration *ConfigurationClass) GetOpenshiftConfig() *openshift.OpenshiftConfig {
+	configuration.init()
+	return configuration.openshiftConfig
 }
 
-func (configurationClass *ConfigurationClass) GetApiClusterIndex() int {
-	configurationClass.init()
-	return configurationClass.apiClusterIndex
+func (configuration *ConfigurationClass) GetApiClusterIndex() int {
+	configuration.init()
+	return configuration.apiClusterIndex
 }
 
-func (configurationClass *ConfigurationClass) GetApiClusterName() string {
-	configurationClass.init()
-	return configurationClass.openshiftConfig.APICluster
+func (configuration *ConfigurationClass) GetApiClusterName() string {
+	configuration.init()
+	return configuration.openshiftConfig.APICluster
+}
+
+func (configuration *ConfigurationClass) GetAffiliation() string {
+	configuration.init()
+	return configuration.openshiftConfig.Affiliation
+}
+
+func (configuration *ConfigurationClass) GetPersistentOptions() *cmdoptions.CommonCommandOptions {
+	configuration.init()
+	return configuration.persistentOptions
 }
