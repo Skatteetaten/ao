7ee157bf418bbfbdaa48461929ac31c612b4ba6a
Håkon Klausen <hakon.klausen@icloud.com>
2017-09-20 10:04:56 +0200

    Bugfix/aos 1801 ao save failes on empty repo (#65)
    * Pull fix
    
    * Fixed ao save and ao pull
    
    * Changed error message

--- pkg/auroraconfig/versioncontrol_test.go
+++ pkg/auroraconfig/versioncontrol_test.go
@@ -3,21 +3,57 @@ package auroraconfig
 import (
 	"fmt"
 	"os"
+	"github.com/skatteetaten/ao/pkg/configuration"
+	"github.com/skatteetaten/ao/pkg/openshift"
+	"github.com/skatteetaten/ao/pkg/cmdoptions"
 	"os/exec"
 	"testing"
 )
 
-func TestValidateRepo(t *testing.T) {
+const REPO_PATH = "/tmp/ao/testRepo"
 
-	checkoutPath := "/tmp/ao/testRepo"
-	os.MkdirAll(checkoutPath, 0755)
-	os.Chdir(checkoutPath)
+var testConfig *configuration.ConfigurationClass = &configuration.ConfigurationClass{
+	OpenshiftConfig: &openshift.OpenshiftConfig{
+		Localhost: true,
+		Affiliation: "aurora",
+	},
+	PersistentOptions: &cmdoptions.CommonCommandOptions{
+		Localhost: true,
+	},
+}
 
-	gitRemoteUrl := fmt.Sprintf(GIT_URL_FORMAT, "user", "aurora")
+func repoSetup(gitRemoteUrl string) {
+	// Clear old test files
+	os.RemoveAll(REPO_PATH)
+	os.MkdirAll(REPO_PATH, 0755)
+	os.Chdir(REPO_PATH)
+
 	exec.Command("git", "init").Run()
 	exec.Command("git", "remote", "add", "origin", gitRemoteUrl).Run()
+}
+
+
+func TestValidateRepo(t *testing.T) {
+	gitRemoteUrl := fmt.Sprintf(GIT_URL_FORMAT, "user", testConfig.GetAffiliation())
+	repoSetup(gitRemoteUrl)
 
 	if err := ValidateRepo(gitRemoteUrl); err != nil {
 		t.Error(err)
+	}
+}
+
+func TestFindGitPath(t *testing.T) {
+	gitRemoteUrl := fmt.Sprintf(GIT_URL_FORMAT, "user", testConfig.GetAffiliation())
+	repoSetup(gitRemoteUrl)
+
+	test := REPO_PATH+"/random/test"
+
+	os.MkdirAll(test, 0755)
+	os.Chdir(test)
+
+	wd, _ := os.Getwd()
+	path, found := FindGitPath(wd)
+	if !found || path != REPO_PATH{
+		t.Error("Expected git repo to be found")
 	}
 }
