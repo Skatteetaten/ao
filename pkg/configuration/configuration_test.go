3360708879e2204c2af542cc86e369371d32c9f1
HaakonKlausen <hakon.klausen@skatteetaten.no>
2017-09-25 10:38:20 +0200

    More tests

--- pkg/configuration/configuration_test.go
+++ pkg/configuration/configuration_test.go
@@ -21,3 +21,35 @@ func TestInit(t *testing.T) {
 		t.Errorf("Expected config location %v, got %v", expectedConfigLocation, config.configLocation)
 	}
 }
+
+func TestGetApiClusterIndex(t *testing.T) {
+	config := NewTestConfiguration()
+	apiClusterIndex := config.GetApiClusterIndex()
+	if apiClusterIndex != 0 {
+		t.Errorf("Did not expect legal API cluster index, got %v", apiClusterIndex)
+	}
+}
+
+func TestGetApiClusterName(t *testing.T) {
+	config := NewTestConfiguration()
+	apiClusterName := config.GetApiClusterName()
+	if apiClusterName != "" {
+		t.Errorf("Did not expect legal API cluster name, got %v", apiClusterName)
+	}
+}
+
+func TestGetAffiliation(t *testing.T) {
+	config := NewTestConfiguration()
+	affiliation := config.GetAffiliation()
+	if affiliation != "" {
+		t.Errorf("Did not expect legal affiliation, got %v", affiliation)
+	}
+}
+
+func TestGetPersistentOptions(t *testing.T) {
+	config := NewTestConfiguration()
+	persistentOptions := config.GetPersistentOptions()
+	if persistentOptions.ShowConfig {
+		t.Errorf("Did not expect ShowConfig")
+	}
+}
