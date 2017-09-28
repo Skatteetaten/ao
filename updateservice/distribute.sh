324423476b1e9234a6ddbf58b665cad932f0173c
Håkon Klausen <hakon.klausen@skatteetaten.no>
2017-09-28 11:00:49 +0200

    Fixed releaselocation

--- updateservice/distribute.sh
+++ updateservice/distribute.sh
@@ -28,20 +28,20 @@ fi
 #
 # Set nodename on OpenShift node used to populate the PV
 #
-distlocation=/home/$USER/nettverksdisker/programvare/Utvikling/ao/
+distlocation=/home/$USER/nettverksdisker/programvare/Utvikling/ao
 case $env in
   "utv")
     openshiftnode=uil0paas-utv-node01
     aorelease=/home/$USER/go/src/github.com/skatteetaten/ao/bin/amd64/ao
-    cp $aorelease $distlocation
+    cp $aorelease $distlocation/
     ;;
   "test")
     openshiftnode=tsl0paas-test-node01
-    aorelease=$distlocation
+    aorelease=$distlocation/ao
     ;;
   "prod")
     openshiftnode=psl0paas-prod-node01
-    aorelease=$distlocation
+    aorelease=$distlocation/ao
     ;;
 esac
 if [ -z $openshiftnode ]; then
