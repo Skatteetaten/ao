439b81b8a56fa037481e30bb38d57c048f639dae
Klausen, Håkon <hakon.klausen@skatteetaten.no>
2018-01-12 10:55:26 +0100

    Added MacOS and Windows download links

--- website/api/index.js
+++
@@ -1,8 +0,0 @@
-// TODO: Denne filen kan slettes når Webleveransepakke støtter kun statiske filer (AOS-2048)
-const http = require('http')
-
-const server = http.createServer((req, res) => {
-    res.end();
-});
-
-server.listen(9090);
