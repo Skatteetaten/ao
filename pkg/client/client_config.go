cd3e77228314358586001205f629868b552bc84f
Rune Offerdal <Rune.Offerdal@skatteetaten.no>
2020-01-23 14:17:12 +0100

    AOS-4026 removing conversion of apiVersion from string to int

--- pkg/client/client_config.go
+++ pkg/client/client_config.go
@@ -1,9 +1,5 @@
 package client
 
-import (
-	"strconv"
-)
-
 type ClientConfig struct {
 	GitUrlPattern string `json:"gitUrlPattern"`
 	ApiVersion    int    `json:"apiVersion"`
@@ -13,10 +9,7 @@ func (api *ApiClient) GetClientConfig() (*ClientConfig, error) {
 	clientConfigGraphqlRequest := `{auroraApiMetadata{clientConfig{gitUrlPattern apiVersion}}}`
 	type ClientConfigResponse struct {
 		AuroraApiMetadata struct {
-			ClientConfig struct {
-				GitUrlPattern string `json:"gitUrlPattern"`
-				ApiVersion    string `json:"apiVersion"`
-			}
+			ClientConfig ClientConfig
 		}
 	}
 
@@ -24,13 +17,7 @@ func (api *ApiClient) GetClientConfig() (*ClientConfig, error) {
 	if err := api.RunGraphQl(clientConfigGraphqlRequest, &clientConfigResponse); err != nil {
 		return nil, err
 	}
-
-	// Need to convert to int for reuse in existing domain logic
-	apiVersion, err := strconv.Atoi(clientConfigResponse.AuroraApiMetadata.ClientConfig.ApiVersion)
-	if err != nil {
-		return nil, err
-	}
-	gc := ClientConfig{clientConfigResponse.AuroraApiMetadata.ClientConfig.GitUrlPattern, apiVersion}
+	gc := clientConfigResponse.AuroraApiMetadata.ClientConfig
 
 	return &gc, nil
 }
