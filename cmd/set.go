53a0a5cd85a31706adc24058b31d1b845f5215ae
Rune Offerdal <Rune.Offerdal@skatteetaten.no>
2020-03-25 12:22:49 +0100

    AOS-4059 Replacing old ao set command with new implementation

--- cmd/set.go
+++ cmd/set.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"github.com/skatteetaten/ao/pkg/auroraconfig"
-	"github.com/skatteetaten/ao/pkg/service"
 	"github.com/spf13/cobra"
 )
 
@@ -20,43 +19,11 @@ var setCmd = &cobra.Command{
 	RunE:        Set,
 }
 
-const setNewExample = `  ao setnew foo.json /pause true
-
-  ao setnew test/about.json /cluster utv
-
-  ao setnew test/foo.json /config/IMPORTANT_ENV 'Hello World'`
-
-var setCmdNewExperimental = &cobra.Command{
-	Use:         "setnew <file> <json-path> <value>",
-	Short:       "Set a single configuration value in the current AuroraConfig",
-	Annotations: map[string]string{"type": "remote"},
-	Example:     setNewExample,
-	RunE:        SetNew,
-}
-
 func init() {
 	RootCmd.AddCommand(setCmd)
-	RootCmd.AddCommand(setCmdNewExperimental)
 }
 
 func Set(cmd *cobra.Command, args []string) error {
-	if len(args) != 3 {
-		return cmd.Usage()
-	}
-
-	name, path, value := args[0], args[1], args[2]
-
-	fileName, err := service.SetValue(DefaultApiClient, name, path, value)
-	if err != nil {
-		return err
-	}
-
-	cmd.Printf("%s has been updated with %s %s\n", fileName, path, value)
-
-	return nil
-}
-
-func SetNew(cmd *cobra.Command, args []string) error {
 	if len(args) != 3 {
 		return cmd.Usage()
 	}
