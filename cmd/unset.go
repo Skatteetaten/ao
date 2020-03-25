9c0773d8527d354a4058ba74fc4e22283522a09b
Rune Offerdal <Rune.Offerdal@skatteetaten.no>
2020-03-25 15:40:09 +0100

    AOS-4059 Unset functionality cleanup

--- cmd/unset.go
+++ cmd/unset.go
@@ -17,59 +17,11 @@ var unsetCmd = &cobra.Command{
 	RunE:        Unset,
 }
 
-const unsetExampleNewExperimental = `  ao unsetnew foo.json /pause
-
-  ao unsetnew test/foo.json /config/IMPORTANT_ENV`
-
-var unsetNewCmd = &cobra.Command{
-	Use:         "unsetnew <file> <json-path>",
-	Short:       "Remove a single configuration entry in the current AuroraConfig",
-	Annotations: map[string]string{"type": "remote"},
-	Example:     unsetExampleNewExperimental,
-	RunE:        UnsetNew,
-}
-
 func init() {
 	RootCmd.AddCommand(unsetCmd)
-	RootCmd.AddCommand(unsetNewCmd)
 }
 
 func Unset(cmd *cobra.Command, args []string) error {
-	if len(args) != 2 {
-		return cmd.Usage()
-	}
-
-	fileNames, err := DefaultApiClient.GetFileNames()
-	if err != nil {
-		return err
-	}
-
-	name := args[0]
-	fileName, err := fileNames.Find(name)
-	if err != nil {
-		return err
-	}
-
-	path := args[1]
-	op := auroraconfig.JsonPatchOp{
-		OP:   "remove",
-		Path: path,
-	}
-
-	if err = op.Validate(); err != nil {
-		return err
-	}
-
-	if err = DefaultApiClient.PatchAuroraConfigFile(fileName, op); err != nil {
-		return err
-	}
-
-	cmd.Printf("%s has been updated\n", fileName)
-
-	return nil
-}
-
-func UnsetNew(cmd *cobra.Command, args []string) error {
 	if len(args) != 2 {
 		return cmd.Usage()
 	}
