d0eaff2cac41c447c147bea24d954ebb1ab2e102
Rune Offerdal <Rune.Offerdal@skatteetaten.no>
2020-05-07 13:09:34 +0200

    AOS-4349 remove glide in jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -16,7 +16,7 @@ type gocov 2> /dev/null || go get github.com/axw/gocov/gocov
 type gocov-xml 2> /dev/null || go get github.com/AlekSi/gocov-xml
 
 
-GOPATH=$GOROOT glide install
+# GOPATH=$GOROOT glide install
 
 export JUNIT_REPORT=TEST-junit.xml
 export COBERTURA_REPORT=coverage.xml
