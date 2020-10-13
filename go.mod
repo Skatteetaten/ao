81602011a5aadb3fbea64c8c05ebd4b08d57cfd1
Rune Offerdal <52204930+RuneOfferdal@users.noreply.github.com>
2020-10-13 12:44:59 +0200

    Feature/aos 4923 graphql client (#189)
    * AOD-4923 changing dependency to forked graphql client for skatteetaten
    
    * AOS-4923 ao calls graphql client that returns error structure
    
    * AOS-4923 update reference to skatteetaten graphql client
    
    * AOS-4923 Parsing of complex error structure with unit test
    
    * AOS-4923 better handling of errors when parsing extensions
    
    * AOS-4923 version of graphql client updated
    
    * AOS-4923 Using more type assigned methods as commented in PR
    
    * AOS-4923 Code review fix

--- go.mod
+++ go.mod
@@ -6,12 +6,12 @@ require github.com/spf13/cobra v1.0.0
 
 require (
 	github.com/andybalholm/crlf v0.0.0-20171020200849-670099aa064f
-	github.com/machinebox/graphql v0.2.2
 	github.com/matryer/is v1.3.0 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/pkg/errors v0.9.1
 	github.com/renstrom/fuzzysearch v1.0.1
 	github.com/sirupsen/logrus v1.5.0
+	github.com/skatteetaten/graphql v0.2.3-0.20201009105426-b4ccc063e40d
 	github.com/stretchr/testify v1.5.1
 	golang.org/x/sys v0.0.0-20190530182044-ad28b68e88f1
 	golang.org/x/text v0.3.2
--- pkg/client/goboapi.go
+++ pkg/client/goboapi.go
@@ -2,9 +2,12 @@ package client
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
-	"github.com/machinebox/graphql"
 	"github.com/sirupsen/logrus"
+	"github.com/skatteetaten/graphql"
+	"strings"
 )
 
 // RunGraphQl performs a GraphQl based API call
@@ -14,7 +17,8 @@ func (api *APIClient) RunGraphQl(graphQlRequest string, response interface{}) er
 	ctx := context.Background()
 
 	if err := client.Run(ctx, req, response); err != nil {
-		return err
+		extractederr := extractGraphqlErrorMsgs(err)
+		return extractederr
 	}
 	return nil
 }
@@ -26,7 +30,8 @@ func (api *APIClient) RunGraphQlMutation(graphQlRequest *graphql.Request, respon
 	graphQlRequest.Header.Add("Authorization", "Bearer "+api.Token)
 
 	if err := client.Run(ctx, graphQlRequest, response); err != nil {
-		return err
+		extractederr := extractGraphqlErrorMsgs(err)
+		return extractederr
 	}
 	return nil
 }
@@ -42,4 +47,121 @@ func (api *APIClient) newRequest(graphqlRequest string) *graphql.Request {
 	req := graphql.NewRequest(graphqlRequest)
 	req.Header.Set("Cache-Control", "no-cache")
 	return req
+}
+
+func extractGraphqlErrorMsgs(errorsInput error) error {
+	if errorsInput != nil {
+		if graphqlErrors, ok := errorsInput.(graphql.Errors); ok {
+			logrus.Debugf("extractGraphqlErrorMsg got %+v", graphqlErrors)
+			errorMsgs := make([]string, len(graphqlErrors))
+			for i, e := range graphqlErrors {
+				errorMsgs[i] = getPrioritizedErrMsg(e)
+			}
+			return errors.New(strings.Join(errorMsgs, "; "))
+		} else {
+			logrus.Warnf("extractGraphqlErrorMsg got ordinary error (deprececated): %s", errorsInput)
+			return errorsInput
+		}
+	}
+	return nil
+}
+
+// Extract message from error by prioritised level
+// 1. error.extensions.errors[...].details[...].message
+// 2. error.extensions.errorMessage
+// 3. error.message (default)
+func getPrioritizedErrMsg(graphqlErr graphql.Error) string {
+	extensions, parseError := parseExtensions(graphqlErr.Extensions)
+	if parseError == nil && extensions != nil {
+		extErrorMsg := extensions.getErrorMessage()
+		if extErrorMsg != "" {
+			return extErrorMsg
+		}
+	}
+	// 3. error.message (default)
+	if graphqlErr.Message == "" {
+		if parseError != nil {
+			return "got unparseable extended error response from server"
+		}
+		return "got unspecified error"
+	}
+	return graphqlErr.Message
+}
+
+func parseExtensions(unparsedExtensions map[string]interface{}) (*extensions, error) {
+	if unparsedExtensions != nil {
+		extensions := extensions{}
+		jsonExtensions, err := json.Marshal(unparsedExtensions)
+		if err != nil {
+			logrus.Warnf("Could not get json from %+v", unparsedExtensions)
+			return nil, err
+		}
+		if err := json.Unmarshal(jsonExtensions, &extensions); err != nil {
+			logrus.Warnf("Could not parse extensions from %s", jsonExtensions)
+			return nil, err
+		}
+		return &extensions, nil
+	}
+	return nil, nil
+}
+
+// Extension extends the standard GraphQl error structure with more application specific details
+type extensions struct {
+	Code           string
+	ErrorMessage   string
+	SourceSystem   string
+	Message        string
+	ExtErrors      []extError `json:"errors"`
+	Classification string
+}
+
+func (ext extensions) getErrorMessage() string {
+	// 1. error.extensions.errors[...].details[...].message
+	if ext.ExtErrors != nil && len(ext.ExtErrors) > 0 {
+		detailMsgs := ext.getDetailsErrorMessages()
+		if len(detailMsgs) > 0 {
+			return strings.Join(detailMsgs, "; ")
+		}
+	}
+	// 2. error.extensions.errorMessage
+	if ext.ErrorMessage != "" {
+		return ext.ErrorMessage
+	}
+	return ""
+}
+
+func (ext extensions) getDetailsErrorMessages() []string {
+	detailMsgs := make([]string, 0)
+	for _, extError := range ext.ExtErrors {
+		detailMsgs = extError.appendDetailsMessages(detailMsgs)
+	}
+	return detailMsgs
+}
+
+type extError struct {
+	Application string
+	Environment string
+	Details     []detail
+	Type        string
+}
+
+func (extError extError) appendDetailsMessages(detailMsgs []string) []string {
+	if extError.Details != nil && len(extError.Details) > 0 {
+		for _, detail := range extError.Details {
+			detailMsgs = detail.appendMessage(detailMsgs)
+		}
+	}
+	return detailMsgs
+}
+
+type detail struct {
+	Type    string
+	Message string
+}
+
+func (detail detail) appendMessage(detailMsgs []string) []string {
+	if detail.Message != "" {
+		detailMsgs = append(detailMsgs, detail.Message)
+	}
+	return detailMsgs
 }
