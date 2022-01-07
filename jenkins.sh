bae4f01b154f6a073ec8e2280ea5a730a9b6bfe0
Rune Offerdal <Rune.Offerdal@skatteetaten.no>
2022-01-07 14:39:58 +0100

    typo

--- jenkins.sh
+++ jenkins.sh
@@ -2,7 +2,7 @@
 
 type go-junit-report 2> /dev/null || go get -d -u github.com/jstemmer/go-junit-report && go install github.com/jstemmer/go-junit-report
 type gocov 2> /dev/null || go get -d github.com/axw/gocov/gocov && go install github.com/axw/gocov/gocov
-type gocov-xml 2> /dev/null || go get -d github.com/AlekSi/gocov-xml && go install ithub.com/AlekSi/gocov-xml
+type gocov-xml 2> /dev/null || go get -d github.com/AlekSi/gocov-xml && go install github.com/AlekSi/gocov-xml
 
 export JUNIT_REPORT=TEST-junit.xml
 export COBERTURA_REPORT=coverage.xml
